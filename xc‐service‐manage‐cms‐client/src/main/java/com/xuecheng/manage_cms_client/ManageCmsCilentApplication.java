a3d89a823a2c5ce010ad200915b0e6709555e67e
woniuaichiluobo <hhhezhao@163.com>
2019-09-01 14:28:09 +0800

    CMS系统页面发布服务

---
+++ xc‐service‐manage‐cms‐client/src/main/java/com/xuecheng/manage_cms_client/ManageCmsCilentApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng.manage_cms_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common下的所有类
+@ComponentScan(basePackages={"com.xuecheng.manage_cms_client"})//扫描本项目下的所有类
+public class ManageCmsCilentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsCilentApplication.class, args);
+    }
+
+}
